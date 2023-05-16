864abdc2502f2c94cff00ed4d0dc060732fabab5
Sabrina Krakau <sabrinakrakau@gmail.com>
2023-05-16 10:16:37 +0200

    Adjusted comments and class description

--- plugins/nf-co2footprint/src/main/nextflow/co2footprint/CO2FootprintConfig.groovy
+++ plugins/nf-co2footprint/src/main/nextflow/co2footprint/CO2FootprintConfig.groovy
@@ -6,14 +6,13 @@ import groovy.transform.PackageScope
 /**
  * This class allows model an specific configuration, extracting values from a map and converting
  *
- * In this plugin, the user can configure how the messages are prefixed with a String, i.e.
- * due a nextflow.config
+ * In this plugin, the user can configure the output file names of the CO2 footprint calculations
  *
  * co2footprint {
- *     prefix = '>>'
+ *     file = "co2footprint.txt"
+ *     summaryFile = "co2footprint.summary.txt"
  * }
  *
- * when the plugin reverse a String it will append '>>' at the beginning instead the default 'Mr.'
  *
  * We anotate this class as @PackageScope to restrict the access of their methods only to class in the
  * same package
@@ -24,23 +23,14 @@ import groovy.transform.PackageScope
 @PackageScope
 class CO2FootprintConfig {
 
-    // final private Boolean enabled
     final private String  file
     final private String  summaryFile
 
     CO2FootprintConfig(Map map){
         def config = map ?: Collections.emptyMap()
-        // if (config.enabled == null ) {
-        //     enabled = true
-        // } else {
-        //     enabled = false
-        //     return
-        // }
         file = config.file ?: CO2FootprintObserver.DEF_FILE_NAME
         summaryFile = config.summaryFile ?: CO2FootprintObserver.DEF_SUMMARY_FILE_NAME
     }
-
-    // Boolean isEnabled() { enabled }
 
     String getFile() { file }
 
