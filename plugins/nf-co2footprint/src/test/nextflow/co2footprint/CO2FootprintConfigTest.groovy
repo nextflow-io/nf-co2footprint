6c4cc9e394439473157cd6146205b946bb48c896
JosuaCarl <josua.carl@uni-tuebingen.de>
2025-03-26 10:38:09 +0100

    Test: Added Test file for configs
    Signed-off-by: JosuaCarl <josua.carl@uni-tuebingen.de>

---
+++ plugins/nf-co2footprint/src/test/nextflow/co2footprint/CO2FootprintConfigTest.groovy
@@ -0,0 +1,44 @@
+package nextflow.co2footprint
+
+import spock.lang.Specification
+
+import java.util.concurrent.ConcurrentHashMap
+
+class CO2FootprintConfigTest extends Specification {
+    def 'config should be rejected for several cases' () {
+        when:
+        new CO2FootprintConfig(input, new TDPDataMatrix())
+
+        then:
+        Exception e = thrown(expectedException)
+        e.getMessage().contains(message)
+
+        where:
+        input                                                   || expectedException        || message
+        ['ci': 100.0, 'location': 'DE']  as ConcurrentHashMap   || IllegalArgumentException || 'Invalid combination of \'ci\' and \'location\''
+        ['location': 'Tuebingen']  as ConcurrentHashMap         || IllegalArgumentException || 'Invalid \'location\' parameter: Tuebingen.'
+    }
+
+    def 'test configuration builder' () {
+        setup:
+        TDPDataMatrix tdp = new TDPDataMatrix(
+                [[1]],
+                ['tdp (W)'] as LinkedHashSet,
+                ['default'] as LinkedHashSet
+        )
+
+        when:
+        CO2FootprintConfig config = new CO2FootprintConfig(input, tdp)
+
+        then:
+        keys.each({property ->
+            config.getProperty(property) == input.get(property)
+        }).every()
+
+        where:
+        input                                               || keys
+        ['ci': 100.0] as ConcurrentHashMap                  || ['ci']
+        ['location': 'DE', 'pue': 2.0] as ConcurrentHashMap || ['location', 'pue']
+
+    }
+}
