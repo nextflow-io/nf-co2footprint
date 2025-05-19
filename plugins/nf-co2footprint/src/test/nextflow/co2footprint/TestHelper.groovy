bef1fc9c8cd9d9f32de71897b4024ba4d9f75934
JosuaCarl <josua.carl@uni-tuebingen.de>
2025-05-19 10:10:17 +0200

    Test: Added `ChecksumChecker` to help with checksum generation and comparison
    Signed-off-by: JosuaCarl <josua.carl@uni-tuebingen.de>

--- plugins/nf-co2footprint/src/test/nextflow/co2footprint/TestHelper.groovy
+++ plugins/nf-co2footprint/src/test/nextflow/co2footprint/TestHelper.groovy
@@ -25,6 +25,9 @@ import java.nio.file.Files
 import java.nio.file.Path
 import java.util.zip.GZIPInputStream
 
+import groovy.json.JsonSlurper
+import java.security.MessageDigest
+
 /**
  *
  * @author Paolo Di Tommaso <paolo.ditommaso@gmail.com>
@@ -42,4 +45,67 @@ class TestHelper {
         return result
     }
 
+}
+
+
+/**
+ * Checksum checker to compare and generate checksums (for files)
+ */
+class ChecksumChecker {
+
+    // Checksums to compare to
+    private Map<String, String> checksums
+
+    /**
+     * Checksum checker from a given Path with a JSON file of checksums
+     *
+     * @param checksumPath JSON file of checksums
+     */
+    ChecksumChecker(Path checksumPath=null) {
+        this.checksums = checksumPath ? loadChecksums(checksumPath) : null
+    }
+
+    /**
+     * Calculates the MD5 checksum for a file
+     *
+     * @param file File to be calculated the checksum to
+     * @return The checksum of the file as a String
+     */
+    static String calculateMD5(File file, List<Integer> excludedLines=[]) {
+        MessageDigest md = MessageDigest.getInstance("MD5")
+        int position = 0
+        file.eachLine("UTF-8") { line ->
+            if (!excludedLines.contains(position)) {
+                byte[] bytes = (line + System.lineSeparator()).getBytes("UTF-8")
+                md.update(bytes)
+            }
+            position += 1
+        }
+        md.digest().encodeHex().toString()
+    }
+
+    /**
+     * Load a JSON file with checksums.
+     *
+     * @param jsonPath Path to the JSON file with checksums
+     * @return The checksums as a Map with String keys and values
+     */
+    Map<String, String> loadChecksums(Path jsonPath=this.class.getResource('/checksums.json').getPath() as Path) {
+        JsonSlurper jsonSlurper = new JsonSlurper()
+        this.checksums = jsonSlurper.parse(jsonPath) as Map<String, String>
+        return this.checksums
+    }
+
+    /**
+     * Compare the recorded checksum to the new checksum.
+     *
+     * @param path
+     * @param checksums
+     * @return
+     */
+    void checkSums(Path path, List<Integer> excludedLines=[], String recordedChecksum=null) {
+        String newChecksum = calculateMD5(path.toFile(), excludedLines)
+        recordedChecksum ?= this.checksums[path.getFileName() as String]
+        assert recordedChecksum == newChecksum
+    }
 }
