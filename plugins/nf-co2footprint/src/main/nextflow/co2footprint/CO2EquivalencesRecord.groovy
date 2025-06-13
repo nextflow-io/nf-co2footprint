f536c5042b30f923a29ab651a73dfbfc585598e3
JosuaCarl <josua.carl@uni-tuebingen.de>
2025-06-13 10:31:22 +0200

    Refactor: Moved adding of percentage to return Readable method
    Signed-off-by: JosuaCarl <josua.carl@uni-tuebingen.de>

--- plugins/nf-co2footprint/src/main/nextflow/co2footprint/CO2EquivalencesRecord.groovy
+++ plugins/nf-co2footprint/src/main/nextflow/co2footprint/CO2EquivalencesRecord.groovy
@@ -34,7 +34,7 @@ class CO2EquivalencesRecord {
     String getTreeMonthsReadable() { Converter.toReadableTimeUnits(treeMonths, 'months', 's', 'years', 0) }
 
     Double getPlanePercent() { planePercent }
-    String getPlanePercentReadable() { Converter.toScientificNotation(planePercent) }
+    String getPlanePercentReadable() { "${Converter.toScientificNotation(planePercent)} %" }
 
     Integer getPlaneFlights() { planePercent / 100 as Integer }
     String getPlaneFlightsReadable() { Converter.toScientificNotation(this.getPlaneFlights()) }
@@ -55,7 +55,7 @@ class CO2EquivalencesRecord {
                     outStr = "- It takes one tree ${this.getTreeMonthsReadable()} to sequester the equivalent amount of CO2 from the atmosphere"
                 case 'planePercent' ->
                     if (value < 100) {
-                        outStr = "- ${this.getPlanePercentReadable()}% of a flight from Paris to London"
+                        outStr = "- ${this.getPlanePercentReadable()} of a flight from Paris to London"
                     }
                     else {
                         outStr = "- ${this.getPlaneFlightsReadable()} flights from Paris to London"
