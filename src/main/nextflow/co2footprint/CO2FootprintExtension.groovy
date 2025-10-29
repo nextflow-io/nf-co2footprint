39f396f841e7af662f80d4c3634665e50ed30f60
JosuaCarl <josua.carl@uni-tuebingen.de>
2025-10-29 14:14:21 +0100

    Feature: Added Extension point for calculating CO2 emissions from trace files
    Signed-off-by: Josua Carl <josua.carl@uni-tuebingen.de>

--- src/main/nextflow/co2footprint/CO2FootprintExtension.groovy
+++ src/main/nextflow/co2footprint/CO2FootprintExtension.groovy
@@ -1,13 +1,18 @@
 package nextflow.co2footprint
 
 import nextflow.Session
+import nextflow.co2footprint.Metrics.Converter
+import nextflow.co2footprint.Metrics.Quantity
 import nextflow.co2footprint.Records.CO2Record
+import nextflow.co2footprint.Records.CO2RecordAggregator
 import nextflow.co2footprint.Records.CiRecordCollector
 import nextflow.plugin.extension.Function
 import nextflow.plugin.extension.PluginExtensionPoint
+import nextflow.processor.TaskId
 import nextflow.trace.TraceRecord
 
 import java.nio.file.Path
+import java.text.SimpleDateFormat
 import java.time.LocalDateTime
 import java.util.concurrent.ConcurrentHashMap
 
@@ -21,17 +26,54 @@ class CO2FootprintExtension extends PluginExtensionPoint {
     }
 
     @Function
-    static TraceRecord parseTraceFile(Path tracePath) {
-        TraceRecord traceRecord = new TraceRecord()
-        return traceRecord.parseTraceFile(tracePath)
+    static List<TraceRecord> parseTraceFile(Path tracePath, String delimiter='\t') {
+        SimpleDateFormat dateParser = new SimpleDateFormat('yyyy-MM-dd HH:mm:ss.SSS')
+        List<String> lines = tracePath.text.readLines()
+        List<String> headers = lines.remove(0).split(delimiter)
+
+        List<TraceRecord> traceRecords = []
+        lines.each { String line ->
+            TraceRecord traceRecord = new TraceRecord()
+            List<String> entries = line.split(delimiter)
+            headers.eachWithIndex { String key, Integer i ->
+                Object value = entries[i]
+                value = switch(TraceRecord.FIELDS.get(key)) {
+                    case 'mem' -> if (value.endsWith('B')) {
+                        List<String> split = value.split(' ')
+                        Quantity quantity = Converter.scaleUnits(split[0].toDouble(), split[1].dropRight(1), 'B', '')
+                        quantity.value.toLong()
+                    }
+                    case 'date' -> { value.matches('\\d+') ? value : dateParser.parse(value).toInstant().toEpochMilli() }
+                    case 'time' -> {
+                        if(value.matches('\\d+')) { value }
+                        else {
+                            String numeric = value.find('\\d+[.]?\\d*')
+                            Quantity quantity = Converter.scaleTime(numeric.toDouble(), value.replace(numeric, ''), 'ms')
+                            quantity.value.toLong()
+                        }
+                    }
+                    case 'perc' -> value.replace('%', '').toDouble()
+                    case 'num' -> value.toDouble()
+                    default -> value
+                }
+                traceRecord.put(key, value)
+            }
+            // Add additional entries
+            traceRecord.put('process', traceRecord.get('name'))
+
+            // Add to collection of trace records
+            traceRecords.add(traceRecord)
+        }
+
+        return traceRecords
     }
 
     @Function
-    CO2Record calculateCO2(
+    List<CO2Record> calculateCO2(
             Path tracePath, Boolean renderFiles=true,
             Double carbonIntensity=null, Map<LocalDateTime, Number> timeCIs=null
     ){
-        TraceRecord traceRecord = parseTraceFile(tracePath)
+        List<TraceRecord> traceRecords = parseTraceFile(tracePath)
 
         if (carbonIntensity) {
             observer.config.set('ci', carbonIntensity)
@@ -40,14 +82,16 @@ class CO2FootprintExtension extends PluginExtensionPoint {
             observer.timeCiRecordCollector = new CiRecordCollector(observer.config, timeCIs as ConcurrentHashMap)
         }
 
-        observer.traceFile.create()
-        observer.startRecord(traceRecord)
-        CO2Record co2Record = observer.aggregateRecords(traceRecord)
-
-        if (renderFiles) {
-            observer.renderFiles()
+        List<CO2Record> co2Records = []
+        traceRecords.each { TraceRecord traceRecord ->
+            observer.aggregator = new CO2RecordAggregator()
+            if (renderFiles) { observer.traceFile.create() }
+            observer.startRecord(traceRecord)
+            co2Records.add(observer.aggregateRecords(traceRecord))
         }
 
-        return co2Record
+        if (renderFiles) { observer.renderFiles() }
+
+        return co2Records
     }
 }
---
+++ src/test/nextflow/co2footprint/CO2FootprintExtensionTest.groovy
@@ -0,0 +1,27 @@
+package nextflow.co2footprint
+
+import nextflow.Session
+import nextflow.co2footprint.Records.CO2Record
+import spock.lang.Shared
+import spock.lang.Specification
+
+import java.nio.file.Path
+
+class CO2FootprintExtensionTest extends Specification {
+    @Shared
+    Session session = new Session([co2footprint: [ci: 100.0]])
+
+    def 'Should calculate the CO2Footprint from an old trace file'() {
+        when:
+        CO2FootprintExtension extension = new CO2FootprintExtension()
+        extension.init(session)
+        List<CO2Record> co2Records = extension.calculateCO2(this.class.getResource('/execution-trace-test.txt').path as Path)
+
+        then:
+        co2Records.size() == 8
+        co2Records[7] == new CO2Record(
+                3.2729169285E-6, 3.2729169285E-4, null, 2.777778E-4, 100.0,
+                1, 11.41, 100.0, 1, 'VALUE_TESTING', null
+        )
+    }
+}
