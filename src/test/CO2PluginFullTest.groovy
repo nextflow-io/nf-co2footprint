18d371a599d2333fe6ddf183db9675a360fe895e
Josua Carl <josua.carl@uni-tuebingen.de>
2026-04-20 13:52:15 +0200

    Test: Adjusted data integration test
    Signed-off-by: Josua Carl <josua.carl@uni-tuebingen.de>

--- src/test/CO2PluginFullTest.groovy
+++ src/test/CO2PluginFullTest.groovy
@@ -90,7 +90,7 @@ class CO2PluginFullTest extends Specification {
         fileChecker.compareNumLines(summaryPath, 30)
         fileChecker.compareNumLines(reportPath, 1863)
         long lines = dataPath.countLines()
-        lines in [1666l, 1667l, 1668l]
+        lines == 3918
     }
 
     def unzip(Path inZip, Path outputDir){
