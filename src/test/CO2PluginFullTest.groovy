9aea23b1c8a14c1eb0d731d073f61edb5020b494
Josua Carl <josua.carl@uni-tuebingen.de>
2026-07-28 11:22:05 +0200

    Test: Adjusted file checks
    Signed-off-by: Josua Carl <josua.carl@uni-tuebingen.de>

--- src/test/CO2PluginFullTest.groovy
+++ src/test/CO2PluginFullTest.groovy
@@ -96,7 +96,7 @@ class CO2PluginFullTest extends Specification {
         // Check line count
         fileChecker.compareNumLines(tracePath, 8)
         fileChecker.compareNumLines(summaryPath, 30)
-        fileChecker.compareNumLines(reportPath, 1842)
+        fileChecker.compareNumLines(reportPath, 1844)
         fileChecker.compareNumLines(dataPath, 4599)
     }
 
