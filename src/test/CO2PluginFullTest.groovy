269116dda5af3fc21c8c1a20c8dbc2c65a1e3f39
nadnein <nadjav.18@gmail.com>
2026-08-07 15:06:54 +0200

    Uupdate integration test
    Signed-off-by: nadnein <nadjav.18@gmail.com>

--- src/test/CO2PluginFullTest.groovy
+++ src/test/CO2PluginFullTest.groovy
@@ -96,8 +96,8 @@ class CO2PluginFullTest extends Specification {
         // Check line count
         fileChecker.compareNumLines(tracePath, 8)
         fileChecker.compareNumLines(summaryPath, 30)
-        fileChecker.compareNumLines(reportPath, 1842)
-        fileChecker.compareNumLines(dataPath, 4599)
+        fileChecker.compareNumLines(reportPath, 1851)
+        fileChecker.compareNumLines(dataPath, 4592)
     }
 
     def unzip(Path inZip, Path outputDir){
